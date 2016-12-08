a1e25863b7aa9f0e5bea9c5d58c947edd4749f48
gregn <gregnb@gmail.com>
2016-12-08 09:16:00 -0500

    Update SimpleRouter.jsx

--- src/SimpleRouter.jsx
+++ src/SimpleRouter.jsx
@@ -34,35 +34,28 @@
 		},
 
 		getInitialState: function() {
-			return { path: null, params: null, root: null, props: null };
-		},
-		
-		compareStrArr: function(source, key, value) {
-				
-			if((!key || key.length == 0))
-				return null;
-		
-			var result = source.filter(function(item) { 
-				if(value.toString() == item[key].toString())
-					return item;
-			});
-				
-			return (result) ? result[0] : null;
-					
+			return { 
+				path: null, 
+				params: null, 
+				root: null, 
+				props: null 
+			};
 		},
 
-		searchSubArr: function(source, key, value) {
-				
+
+		arrFind: function(type, source, key, value) {
+
 			if((!key || key.length == 0))
 				return null;
 			
 			var result = source.filter(function(item) { 
-				if(value.lastIndexOf(item[key], 0) === 0)
+				if((type == 'compare' && value.lastIndexOf(item[key], 0) === 0) || 
+					(type == 'search' && value.toString() == item[key].toString()))
 					return item;
 			});
 				
 			return (result) ? result[0] : null;
-					
+		
 		},
 		
 		changeRoute: function(path, params, props) {
@@ -70,6 +63,172 @@
 			var url = this.state.root+path
 				url = url.replace(/\/\/+/g, '/'); /* strip possible user error double slashes */
 			
+			var result = {};
+			
+			if(url == '/*') {
+				
+				/* useful for welcome/default index screen */			
+				var routeInfo = this.arrFind('compare', this.routingMap, 'path', '/*');
+
+				result.path = routeInfo.path;
+				result.component = (routeInfo.component) ? routeInfo.component : null ;
+				result.params = null;
+				
+				url = '/';
+							
+			} else {
+				
+				if(params)
+					for(var i = 0; i < params.length; i++)
+						if(params[i].length)
+							url += '/' + params[i];
+				
+				var routeInfo = this.arrFind('search', this.routingMap, 'path', url);
+
+				/* check if there's an index */			
+				if(!routeInfo)
+					routeInfo = this.arrFind('compare', this.routingMap, 'path', url+'/*');
+					
+				result.path = path;
+				result.component = (routeInfo.component) ? routeInfo.component : null ;
+				result.params = params;
+				
+			}
+			
+			this.setState({ 
+				path: result.path,
+				params: result.params,
+				props: (props) ? props : null,
+				component: result.component
+			});
+			
+			history.pushState({ path: url}, '', url);
+			this.saveHistory(path, params);
+						
+		},
+
+		routerHistory: function(limit) {
+			var history = this.historyStack;
+			return (limit) ? history.slice(Math.max(history.length - limit, 0)) : history;
+		},
+				
+		saveHistory: function(url, params) {
+			this.historyStack.push({path: url, params: params});
+		},
+		
+		handlePopState: function(event) {
+			
+			if(event && event.state && event.state.path)
+				this.setPath(event.state.path);
+				
+		},
+		
+		/* crawl all <Route /> nodes to build a complete routing map */
+		buildRoutes: function(child, depth, path) {
+			
+			if (!child || !child.props) 
+				return;
+		
+			/* if there's a rendering component THEN add it to the map */
+			if(child.props.component)
+				this.routingMap.push({path: this.props.root+((path)?path:'')+child.props.path, component: child.props.component });
+			
+			React.Children.forEach(child.props.children, function(item) {				
+				this.buildRoutes(item, depth + 1, child.props.path);			
+			}, this);
+
+		},
+		
+		setPath: function(url, callback) {
+			
+			var path = url;
+				path = path.replace(/\/\/+/g, '/');  			/* remove double slashes */				
+				path = path.replace(this.props.root,'');			/* remove root */
+				path = path.replace(/\/+$/, '');					/* remove trailing */
+				path = (path.length) ? path.split('/') : null;
+		
+			var result = {};
+			
+			if(path) {
+				
+				var searchPath = (this.props.root + path.join('/'));
+				var routeInfo = this.arrFind('search', this.routingMap, 'path', searchPath);
+				
+				/* check if there's an index */			
+				if(!routeInfo)
+					routeInfo = this.arrFind('compare', this.routingMap, 'path', searchPath+'/*');
+				
+				path = searchPath.replace(routeInfo.path, '');
+				result.path = routeInfo.path;
+				
+				result.component = (routeInfo.component) ? routeInfo.component : null ;
+			
+				path = path.replace(/\/\/+/g, '/');
+				path = path.split('/');
+				path.shift();
+				
+				result.params = path;
+				
+			} else {
+
+				var path = this.props.root;
+				path = path.replace(/\/+$/, '');		
+						
+				var routeInfo = this.arrFind('compare', this.routingMap, 'path', path + '/*');
+
+				result.path = routeInfo.path;
+				result.component = (routeInfo.component) ? routeInfo.component : null ;
+				result.params = null;
+	
+			}
+			
+			this.setState({
+				path: (result.path) ? result.path : '/', 
+				params: result.params, 
+				root: this.props.root,
+				component: result.component
+			}, (callback) ? callback : null);
+			
+		},
+		
+		componentDidMount: function() {
+
+			/* init. history stack w/ first URL visited */
+			this.historyStack = Array();
+	
+			/* init. routing map */
+			this.routingMap = Array();
+			this.buildRoutes(this, 0, '');
+			
+			this.setPath(window.location.pathname, function() {
+				this.saveHistory(this.state.path, this.state.params);
+			});
+			
+			window.addEventListener('popstate', this.handlePopState);
+			window.router = this;
+			
+		},
+		
+		render: function() {
+			
+			if(!this.state.path)
+				return false;
+			
+			window.scrollTo(0, 0);
+			return (this.state.component) ? React.cloneElement(this.state.component, this.state.props) : false;
+				
+		}
+		
+ 	});
+
+	/* just a dummy stateless component */
+	var Route = React.createClass({
+
+		render: function() {
+			return false;
+		},
+		
+	});
 			var result = {};
 			
 			if(url == '/*') {
